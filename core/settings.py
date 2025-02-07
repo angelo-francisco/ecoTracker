10b7b10ce88c63176dd4bb37f42c53d26e745c2f
angelo-francisco <ics20080729@gmail.com>
2025-02-08 00:28:34 +0100

    ready for deploy v9

--- core/settings.py
+++ core/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["https://core-58rc.onrender.com", "127.0.0.1"]
+ALLOWED_HOSTS = ["core-58rc.onrender.com", "127.0.0.1", "localhost"]
 
 
 # Application definition
