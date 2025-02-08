d041ff848bb457cee121f9a3c3027eda127faf38
angelo-francisco <ics20080729@gmail.com>
2025-02-08 09:02:46 +0100

    ready for deploy v12

--- core/settings.py
+++ core/settings.py
@@ -128,4 +128,5 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
+    "https://eco-track-fronted.vercel.app"
 ]
